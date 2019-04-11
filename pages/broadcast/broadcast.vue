4f0a9d62a3a87a27d89d5f87d972f77e333567f1
zcc <1285116491@qq.com>
2019-04-11 15:07:29 +0800

    dev0.1

--- pages/broadcast/broadcast.vue
+++ pages/broadcast/broadcast.vue
@@ -1,5 +1,14 @@
 <template>
 	<view>
+		<view class="tab-box">
+			<view class="active chose-tab">关注</view>
+			<view class="chose-tab">推荐</view>
+			<view class="chose-tab">热门</view>
+			<view class="chose-tab">最新</view>
+			<view class="img-add">
+				<image src="" mode=""></image>
+			</view>
+		</view>
 		
 	</view>
 </template>
@@ -15,5 +24,13 @@
 </script>
 
 <style>
+	.tab-box{
+		display: flex;
+		text-align: center;
+		width: 100%;
+	}
+	.chose-tab{
+		flex: 2;
+	}
 
 </style>
